1b5361684a43da26363ac2b3fc18184ef1df6705
Al3xiel <gabriel280104casas@gmail.com>
2024-11-06 15:12:39 -0500

    chore: setup project.

---
+++ src/main/java/pe/medelect/platform/u202220033/Si729pc2u202220033Application.java
@@ -0,0 +1,13 @@
+package pe.medelect.platform.u202220033;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Si729pc2u202220033Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Si729pc2u202220033Application.class, args);
+    }
+
+}
---
+++ src/test/java/pe/medelect/platform/u202220033/Si729pc2u202220033ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.medelect.platform.u202220033;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Si729pc2u202220033ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
