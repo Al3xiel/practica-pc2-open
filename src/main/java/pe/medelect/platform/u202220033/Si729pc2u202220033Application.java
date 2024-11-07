6f3d5083c8ec7a1b7942cb60e99e2695e9028bbf
Al3xiel <gabriel280104casas@gmail.com>
2024-11-06 20:10:02 -0500

    chore: enabled JOA auditing.

--- src/main/java/pe/medelect/platform/u202220033/Si729pc2u202220033Application.java
+++ src/main/java/pe/medelect/platform/u202220033/Si729pc2u202220033Application.java
@@ -2,8 +2,10 @@ package pe.medelect.platform.u202220033;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class Si729pc2u202220033Application {
 
     public static void main(String[] args) {
