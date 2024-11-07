54f9b5a0860c1615627d75655000a35463ee4e83
Al3xiel <gabriel280104casas@gmail.com>
2024-11-06 21:10:55 -0500

    fix(enum): added id to each enum.

--- src/main/java/pe/medelect/platform/u202220033/work/domain/model/valueobjects/WorkType.java
+++ src/main/java/pe/medelect/platform/u202220033/work/domain/model/valueobjects/WorkType.java
@@ -1,8 +1,16 @@
 package pe.medelect.platform.u202220033.work.domain.model.valueobjects;
 
 public enum WorkType {
-    PREVENTIVE_MAINTENANCE,
-    CORRECTIVE_MAINTENANCE,
-    PARAMETER_VERIFICATION,
-    REPOWERING
+    PREVENTIVE_MAINTENANCE(1, "PREVENTIVE_MAINTENANCE"),
+    CORRECTIVE_MAINTENANCE(2, "CORRECTIVE_MAINTENANCE"),
+    PARAMETER_VERIFICATION(3, "PARAMETER_VERIFICATION"),
+    REPOWERING(4, "REPOWERING");
+
+    private final int id;
+    private final String description;
+
+    WorkType(int id, String description) {
+        this.id = id;
+        this.description = description;
+    }
 }
